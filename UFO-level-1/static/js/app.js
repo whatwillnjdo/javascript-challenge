f06a4926e43740ea137e8571a969b13a6ce755f1
NJ <nj@Nirmals-MBP.zyxel.com>
2020-03-13 23:31:19 -0400

    NJ - Added some filtering logic

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -26,32 +26,62 @@ button.on("click", function() {
   // Select the city input element and get the raw HTML node
   var inputCityElement = d3.select("#city");
 
+  // Select the state input element and get the raw HTML node
+  var inputStateElement = d3.select("#state");
+
   // Get the date value property of the input element
   var inputDateValue = inputDateElement.property("value");
 
   // Get the value property of the input element
   var inputCityValue = inputCityElement.property("value");
 
+  // Get the value property of the input element
+  var inputStateValue = inputStateElement.property("value");
+
+
   //Filter only the date
   var filteredDate = tableData.filter(record => record.datetime === inputDateValue);
   //Filter only the city
   var filteredCity = tableData.filter(record => record.city === inputCityValue);
+  //Filter only the state
+  var filteredState = tableData.filter(record => record.state === inputStateValue);
 
-  //if(inputCityValue.length !== 0){
-  //  var filteredCity = tableData.filter(record => record.city === record.city);
-  //}
-
-  console.log(filteredCity.length);
-
-  //Filter by both City and Date
-  var filteredData = (filteredDate && filteredCity)
-
-  //console.log(filteredData);
-
-
-  tbody.html("");
-  populateTable(filteredData);
-
+  if(inputDateValue.length !== 0 && inputCityValue.length !== 0 && inputStateValue.length !== 0){
+    var filteredData = (filteredDate && filteredCity && filteredState);
+    tbody.html("");
+    populateTable(filteredData);
+  }
+  else if(inputDateValue.length !== 0 && inputCityValue.length == 0 && inputStateValue.length == 0){
+    tbody.html("");
+    populateTable(filteredDate);
+  }
+  else if(inputDateValue.length !== 0 && inputCityValue.length !== 0 && inputStateValue.length == 0){
+    var filteredData = (filteredDate && filteredCity)
+    tbody.html("");
+    populateTable(filteredData);
+  }
+  else if(inputDateValue.length !== 0 && inputCityValue.length == 0 && inputStateValue.length !== 0){
+    var filteredData = (filteredDate && filteredState)
+    tbody.html("");
+    populateTable(filteredData);
+  }
+  else if(inputDateValue.length == 0 && inputCityValue.length !== 0 && inputStateValue.length !== 0){
+    var filteredData = (filteredCity && filteredState)
+    tbody.html("");
+    populateTable(filteredData);
+  }
+  else if(inputDateValue.length == 0 && inputCityValue.length !== 0 && inputStateValue.length == 0){
+    tbody.html("");
+    populateTable(filteredCity);
+  }
+  else if(inputDateValue.length == 0 && inputCityValue.length == 0 && inputStateValue.length !== 0){
+    tbody.html("");
+    populateTable(filteredState);
+  }
+  else {
+    tbody.html("");
+    tbody.append("tr").append("td").text("Invalid Filters !! Reset and try again.");
+  }
 
 });
 
